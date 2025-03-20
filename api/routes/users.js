8629705b7a61ce8d3d4ec8123a2dd39deb5aaa22
Tijani Moro <tjhackx111@gmail.com>
2025-03-20 06:24:53 +0000

    initial commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -50,4 +50,31 @@ router.get('/loggedInUser', userAuth.authenticate, UserController.loggedInUser()
 // Usage (example): GET /api/v1/logout
 router.get('/logout', userAuth.authenticate, UserController.logout()); // Logout route - authenticate middleware adds token to req.token
 
+// Usage (example): POST /api/v1/forgot-password
+/** body
+ * {
+        "email": "", 
+        "pin": "",
+    }
+*/
+router.post('/forgot-password', UserController.forgetPassword()) // Forget password 
+
+// Usage (example): POST /api/v1/reset-verify
+/** body
+  * {
+        "code": "", 
+    }
+*/
+router.post('/reset-verify', UserController.verifyVerificationCode()) // reset verify (6 Digit Verification Code) 
+
+// Usage (example): POST /api/v1/reset-password
+/** body
+ * {
+        "pin": "",
+        "password": "",
+        "confirm_password": "", 
+    }
+*/
+router.post('/reset-password', UserController.resetPassword()) // reset password
+
 module.exports = router;
