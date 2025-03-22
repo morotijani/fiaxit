b77ef32d5af1c49a0f1f3ac8b5701ae0096765b2
Tijani Moro <tjhackx111@gmail.com>
2025-03-22 22:16:01 +0000

    initial commit

---
+++ api/routes/auth.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const AuthController = require('../controllers/users-controller');
+const userAuth = require("../middleware/check-auth");
+
+// Usage (example): POST /api/v1/signup
+/** body
+ * {
+        "fname": "",
+        "mname": "", // not compulsory
+        "lname": "",
+        "email": "",
+        "phone": "",
+        "password": "",
+        "confirm_password": "", 
+        "pin": "",
+        "invitationcode": "" // not compulsory
+    }
+*/
+router.post('/signup', AuthController.signup()); // Register users
+
+// Usage (example): POST /api/v1/login
+/** body
+ * {
+        "email": "",
+        "password": "",
+    }
+*/
+router.post('/login', AuthController.login()); // login user
+
+// Usage (example): GET /api/v1/loggedInUser
+router.get('/loggedInUser', userAuth.authenticate, AuthController.loggedInUser()); // logged in user
+
+// Usage (example): GET /api/v1/logout
+router.get('/logout', userAuth.authenticate, AuthController.logout()); // Logout route - authenticate middleware adds token to req.token
+
+// Usage (example): POST /api/v1/forgot-password
+/** body
+ * {
+        "email": "", 
+    }
+*/
+router.post('/forgot-password', AuthController.forgetPassword()) // Forget password 
+
+// Usage (example): POST /api/v1/verify-reset-code
+/** body
+  * {
+        "email": "", 
+        "code": ""
+    }
+*/
+router.post('/verify-reset-code', AuthController.verifyResetCode()) // Endpoint to verify the code and allow password reset (6 Digit Verification Code) 
+
+// Usage (example): POST /api/v1/reset-password
+/** body
+ * {
+        "password": "",
+        "confirm_password": "", 
+    }
+*/
+router.post('/reset-password', AuthController.resetPassword()) // reset password
+
+module.exports = router;
